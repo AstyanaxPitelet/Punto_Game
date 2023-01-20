2847a17e50f86b95b02fd17a3720f9e65a7c0995
Pitelet Astyanax <astyanax.pitelet@gmail.com>
2023-01-20 16:15:26 +0100

    Installation de React

--- app.js
+++ app.js
@@ -6,3 +6,23 @@ const path = require('path');
 const app = express();
 
 mongoose.connect('mongodb://127.0.0.1/puntogame');
+
+app.get('/', (req, res) => {
+    
+})
+
+app.use((req, res) => {
+    res.status(404);
+    res.send('Page non trouvée');
+})
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500);
+    res.send('Erreur interne du serveur');
+});
+
+app.listen(3000, () => {
+    console.log(`Application lancée sur le port 3000`);
+});
+
