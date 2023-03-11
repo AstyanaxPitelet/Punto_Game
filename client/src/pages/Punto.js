a11a22b9bea53b17087888ce90bdbaafb8dd354d
Pitelet Astyanax <astyanax.pitelet@gmail.com>
2023-03-11 20:32:05 +0100

    Punto 0.0.3

--- client/src/pages/Punto.js
+++ client/src/pages/Punto.js
@@ -44,7 +44,6 @@ export default function Punto() {
                     } else {
                         return
                     }
-                    
                 }
                 e.target.appendChild(card) 
             }
