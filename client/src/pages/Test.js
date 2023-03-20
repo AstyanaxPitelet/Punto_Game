8d6807301bffdccbbb6ed8dc9c28eb4b4a2ecc72
Pitelet Astyanax <astyanax.pitelet@gmail.com>
2023-03-20 15:59:11 +0100

    Supression de debug

--- client/src/pages/Test.js
+++ client/src/pages/Test.js
@@ -56,57 +56,6 @@ export default function Test() {
        }
     }, [rule])
 
-    // useEffect(() => {
-    //     let isDone = true
-    //     axios.post(`${api}/rule/player`, {
-    //         nbPlayer: players.length
-    //     }).then((reponse) => {
-    //         if(isDone) {
-    //             setRule(reponse.data)
-    //         }
-    //         setRule(reponse.data)
-    //         if(reponse.data.contrainte.isN) {
-    //             const value = reponse.data.contrainte.color
-    //             axios.post(`${api}/card/color`, {
-    //                 color: value
-    //             }).then((card) => {
-    //                 setCards(cards => [...cards ,card.data]) 
-    //                 setNeutral(card.data)
-    //                 // Un tableau de 18 cartes
-    //                 // Un tableau de 3 joueurs 
-    //                 // Le tableau de joueur attend de recevoir les cartes
-    //                 // Il faut 6 cartes par joueurs
-    //             })
-    //         }
-    //         reponse.data.colors.forEach((color, index) => {
-    //             axios.post(`${api}/card/color`, {
-    //                 color: color
-    //             }).then((card) => {
-    //                 setCards(cards => [...cards ,card.data]) 
-    //                 setPlayers((prev) => {
-    //                     return prev.map((player) => {
-    //                         if(player.id===index) {
-    //                             return {
-    //                                 ...player,
-    //                                 cards: card.data
-    //                             }
-    //                         } else {
-    //                             return player
-    //                         }
-    //                     })
-    //                 })
-                    
-    //                 // players[index].cards = card.data
-    //             })
-    //         })
-
-    //     })
-
-    //     return () => {
-    //         isDone=false
-    //     }
-    // }, [players])
-
     return (
         <div>
             {players.map((player, idx) => (
