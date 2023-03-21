bae4117dd3f6d168c762b3a0a35691ac8f1507cc
Pitelet Astyanax <astyanax.pitelet@gmail.com>
2023-03-21 15:42:35 +0100

    Sécuration des routes

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Punto from './pages/Punto';
 import Login from './components/connexion/Login';
 import Register from './components/connexion/SignUp';
 import Test from './pages/Test';
+import { RequireAuth } from 'react-auth-kit';
 
 function App() {
 
@@ -14,7 +15,11 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/punto" element={<Punto />} />
+          <Route path="/punto" element={
+            <RequireAuth loginPath='/login'>
+              <Punto />
+            </RequireAuth>
+          }/>
           <Route path="/test" element={<Test />} />
         </Routes>
       </>
